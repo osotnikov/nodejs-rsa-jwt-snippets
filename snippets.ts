4bfb999d19bc48292a4f96adc638ec143635a832
osotnikov <alex28_501@hotmail.com>
2020-10-07 17:22:24 +0100

    Create snippets.ts

---
+++ snippets.ts
@@ -0,0 +1,16 @@
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
+import * as path from 'path';
+
+let jsonPath = path.join(__dirname, '.', 'testdata', 'keys', 'jwt_private_key.pk');
+const privateKey = fs.readFileSync(jsonPath).toString('utf-8');
+mockedJwt["exp"] = new Date().getTime() + 3600000; // set it tp expire in one hour
+const signedToken = jwt.sign(mockedJwt, privateKey, { algorithm: 'RS256'});
+jsonPath = path.join(__dirname, '.', 'testdata', 'keys', 'jwt_public_key.pub');
+const publicKey = fs.readFileSync(jsonPath).toString('utf-8');
+// verify a token asymmetric
+let verified = null;
+jwt.verify(signedToken, publicKey, function(err, decoded) {
+  verified = decoded;
+});
+TOKEN = 'Bearer ' + signedToken;
